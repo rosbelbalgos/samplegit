e872f449df38ad330a44f95f0ec75ff4c85410f9
Rosbel Balgos <rosbelbalgos@satellite.com.ph>
2016-04-06 19:24:07 +0800

    Added 7:23pm

--- Testing GIT/src/com/newpack/qa/Try.java
+++ Testing GIT/src/com/newpack/qa/Try.java
@@ -9,6 +9,7 @@ class Try {
 		x = x + 1;
 	}
 	if (x == 4){
+		System.out.println("Conflict at 7:23");
 		System.out.println("Conflict at 7:15");
 		System.out.println("2222222222222");
 		System.out.println("7:19");
