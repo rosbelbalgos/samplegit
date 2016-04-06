0acc963ac18c8e6d02b6e8ffffd9594bd33bc1c6
marvin villaruel <marvin.villaruel@safesat.com.ph>
2016-04-06 15:29:09 +0800

    #001 - Delete refactored code

--- Testing GIT/src/Dog.java
+++ Testing GIT/src/Dog.java
@@ -3,18 +3,6 @@ public class Dog {
 
 	
 	public static void main (String[] args){
-		outputMessage();
-		System.out.println("Added 1:24 pm");
-		System.out.println("Added 1:34 pm");
-		System.out.println("Added 1:41 pm");
-	}
-
-	private static void outputMessage() {
-		System.out.println("Changed this description");
-		System.out.println("Test conflict");
-		System.out.println("Added 1:24 pm");
-		System.out.println("Added 1:34 pm");
-
 		System.out.println("THIS IS MARVIN");
 		System.out.println("Adding a message");
 	}
