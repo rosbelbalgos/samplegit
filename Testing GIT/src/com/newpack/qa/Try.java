c5b2f6d0923480b661646c0c43901a6b53482fdd
marvin villaruel <marvin.villaruel@safesat.com.ph>
2016-04-06 14:01:45 +0800

    #001 - Add new class

---
+++ Testing GIT/src/com/newpack/qa/Try.java
@@ -0,0 +1,14 @@
+package com.newpack.qa;
+
+class IfElseLoop {
+	public static void main (String[] args) {
+	int x = 1;
+	while (x < 4){
+		System.out.println("Doo");
+		System.out.println("Bee");
+		x = x + 1;
+	}
+	if (x == 4){
+		System.out.println("Tama ang iyong ginawa");
+	}
+		}}
