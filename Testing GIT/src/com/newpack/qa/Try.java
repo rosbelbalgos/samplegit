d9553c710088884f7e8c56e772c2f758720b0056
marvin villaruel <marvin.villaruel@safesat.com.ph>
2016-04-06 19:20:34 +0800

    Added 7:20

--- Testing GIT/src/com/newpack/qa/Try.java
+++ Testing GIT/src/com/newpack/qa/Try.java
@@ -9,6 +9,7 @@ class Try {
 		x = x + 1;
 	}
 	if (x == 4){
+		System.out.println("7:20");
 		System.out.println("Conflict at 7:15");
 		System.out.println("2222222222222");
 		// push ==========> 1
