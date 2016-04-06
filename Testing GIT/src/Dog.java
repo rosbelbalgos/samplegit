539004db15e2845e8ddf1fc7ae2e55e24982fef7
Rosbel Balgos <rosbelbalgos@satellite.com.ph>
2016-04-06 11:44:59 +0800

    #0005 - Added time on description

--- Testing GIT/src/Dog.java
+++ Testing GIT/src/Dog.java
@@ -5,5 +5,7 @@ public class Dog {
 	public static void main (String[] args){
 //		System.out.println("Changed this description");
 //		System.out.println("Changed this description");
+		
+		System.out.println("Comment 11:44 am");
 	}
 }
