fa1a1f2f37801f8535eff6bc5f425c59e319b91c
marvin villaruel <marvin.villaruel@safesat.com.ph>
2016-04-06 18:07:28 +0800

    #001 - Try push conflict found

--- Testing GIT/src/Dog.java
+++ Testing GIT/src/Dog.java
@@ -4,6 +4,14 @@ public class Dog {
 	
 	public static void main (String[] args){
 
+<<<<<<< HEAD
+=======
+	}
+
+	private static void outputMessage() {
+		System.out.println("Changed this description");
+		System.out.println("Test conflict");
+>>>>>>> stash
 
 		System.out.println("THIS IS MARVIN");
 		System.out.println("Adding a message");
