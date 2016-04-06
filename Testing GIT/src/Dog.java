501d1cb44f88c1a2bea1cac40343a72694af667b
marvin villaruel <marvin.villaruel@safesat.com.ph>
2016-04-06 18:10:56 +0800

    #001 - merge conflic code found

--- Testing GIT/src/Dog.java
+++ Testing GIT/src/Dog.java
@@ -3,15 +3,8 @@ public class Dog {
 
 	
 	public static void main (String[] args){
-
-<<<<<<< HEAD
-=======
-	}
-
-	private static void outputMessage() {
 		System.out.println("Changed this description");
 		System.out.println("Test conflict");
->>>>>>> stash
 
 		System.out.println("THIS IS MARVIN");
 		System.out.println("Adding a message");
