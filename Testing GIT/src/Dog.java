ed8266cdcb66da989119ae8f42633b4ec7e11087
Rosbel Balgos <rosbelbalgos@satellite.com.ph>
2016-04-06 18:18:42 +0800

    #4444 removed stash

--- Testing GIT/src/Dog.java
+++ Testing GIT/src/Dog.java
@@ -3,13 +3,11 @@ public class Dog {
 
 	
 	public static void main (String[] args){
-<<<<<<< HEAD
 
 	}
 
 	private static void outputMessage() {
-=======
->>>>>>> 501d1cb44f88c1a2bea1cac40343a72694af667b
+
 		System.out.println("Changed this description");
 		System.out.println("Test conflict");
 
